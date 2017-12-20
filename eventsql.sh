f6d617185097fd763f6d6f366f28576aa3d086c3
John G. Alvord <johngrahamalvord@gmail.com>
2017-12-19 17:26:14 -0800

    add data capture scripts

---
+++ eventsql.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Set CANDLEHOME before starting or alter the next line to set installation directory for TEPS
+if [ "$CANDLEHOME" = "" ]; then export CANDLEHOME=/opt/IBM/ITM; fi
+${CANDLEHOME}/bin/itmcmd execute cq 'KfwSQLClient /e "SELECT SITNAME,SITINFO,REEV_DAYS,REEV_TIME,PDT FROM O4SRV.TSITDESC"' >QA1CSITF.DB.LST
+${CANDLEHOME}/bin/itmcmd execute cq 'KfwSQLClient /e "SELECT ID,LSTDATE,FULLNAME FROM O4SRV.TNAME"' >QA1DNAME.DB.LST
+${CANDLEHOME}/bin/itmcmd execute cq 'KfwSQLClient /e "SELECT SITNAME,DELTASTAT,ORIGINNODE,LCLTMSTMP,NODE,ATOMIZE FROM O4SRV.TSITSTSH"' >QA1CSTSH.DB.LST
